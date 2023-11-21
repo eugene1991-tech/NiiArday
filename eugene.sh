1632411b9471871612a0db7d68b5cf153ddd1b62
Nii Arday <arday2009@gmail.com>
2023-11-21 18:39:03 +0000

    Added to content to file

---
+++ eugene.sh
@@ -0,0 +1,43 @@
+## Creating a Remote Repo ####
+=======================================================================
+steps
+=======================================================================
+You will have to create a remote repo on your github account. 
+
+-- Click on new repository
+-- give it a name 
+-- give it a description
+-- You can choose either public or private repo 
+-- initialize with a readme file 
+-- create repo 
+
+--= You have created your git repository
+-- Copy the http link of the repo 
+
+Once you're done, copy the https url and run:
+==> git remote add gitclassrepo https://github.com/richkingss/gitclassrepo.git
+
+==> git remote 
+# This will show all your remote repositories
+==================================================================
+
+NB: Create a token on github before you link the local repo 
+to the  remote repo. In order to do this, we will need our github username and password. 
+=============================================================
+steps to get your access token as password.. 
+============================================================
+
+-- in github, go to setting
+-- go to developer settings 
+-- click on personal access tokens, and select classic tokens
+-- click on generate new tokens
+-- give it a name. 
+-- set it for 30 days  -- all repo 
+-- generate token
+
+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
+
+==> git push NiiArday master
+=============================================================
+
+learn how to create a repository.
